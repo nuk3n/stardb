882f30905f90d339b83bb7df435c18ba3d1ce027
nuk3n <denyanizovkin@gmail.com>
2023-01-11 12:05:21 +0300

    just uploaded css loader

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,21 @@
+import './spinner.css'
+import React from "react";
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-double-ring-98wpwlula0i">
+            <div className="ldio-zz2tkxsk7wm">
+                <div></div>
+                <div></div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Spinner;
